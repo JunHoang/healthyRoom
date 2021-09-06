fcd2d4ae53343b75d7af5581e7a6bd1c2ebdafcb
DungHoang <kathymaidung@gmail.com>
2021-09-06 19:15:33 +0300

    add body abd BlogItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/header"
+import Blog from "../components/blog"
 
 const Home = () => {
   return (
     <Layout>
       <Header />
+      <Blog />
     </Layout>
   )
 }
